43ada9aaac8df47348c825e0cda8d5a0684ec046
Kristy Ho <kristy02ho@gmail.com>
2024-03-31 19:59:03 -0600

    assignment 4

--- Movies.js
+++ Movies.js
@@ -4,9 +4,23 @@ var Schema = mongoose.Schema;
 mongoose.connect(process.env.DB);
 
 // Movie schema
-var MovieSchema = new Schema({
+const MovieSchema = new mongoose.Schema({
+    title: { type: String, required: true, index: true },
+    releaseDate: {type: String, required: true},
+    genre: {
+      type: String,
+      enum: [
+        'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+      ],
+      required: true
+    },
+    actors: [{
+      actorName: {type: String, required: true},
+      characterName: {type: String, required: true},
+    }],
+  }); 
 
-});
+  
 
 // return the model
 module.exports = mongoose.model('Movie', MovieSchema);
--- Users.js
+++ Users.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
+require('dotenv').config();
 
 mongoose.Promise = global.Promise;
 
